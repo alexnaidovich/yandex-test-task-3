3b775728f7f60f16d970da7a54a768538dceaf5d
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-02-11 23:11:19 +0300

    0.2.1
    Worked on test architecture
    
    Co-Authored-By: Skripko Sergey <srjkaa@users.noreply.github.com>

--- js/main.js
+++ js/main.js
@@ -30,50 +30,70 @@ Game.prototype = {
             this.innerText = "РЕСТАРТ";
         });
         console.log(this.citiesChecked);
-        this.listen(this.playerInput);
+        this.listen();
     },
     
-    listen: function(val) {
+    listen: function() {
         console.log(this);
         console.log(this.citiesChecked);
         console.log(this.submit);
         this.submit.addEventListener('click', function(e) {
             e.preventDefault();
-            if (Game.citiesChecked.length === 0) {
-                Game.citiesChecked.push(val);
+            console.log(this);
+            var val = Game.prototype.input.value;
+            var lastLetter = "",
+                lastElem = "",
+                firstLetter = val[0].toLowerCase();
+            
+            var citiesChecked = Game.prototype.citiesChecked
+            if (Game.prototype.AIResponse === "") {
+            if (citiesChecked.length === 0) {
+                Game.prototype.AIResponseField.innerText = val;
+                citiesChecked.push(val);
+//                ymaps.ready(function(){
+//                    var myGeocoder = ymaps.geocode(val);
+//                    myGeocoder.then(
+//                        function (res) {
+//                            zoom: 3;
+//                            alert('Координаты объекта :' + res.geoObjects.get(0).geometry.getCoordinates());
+//                        },
+//                        function (err) {
+//                            alert('Ошибка');
+//                        }
+//                    );
+//                })
+                Game.prototype.input.value = "";
                 return true;
             } else {
-                for (var cities in Game.citiesChecked) {
-                    if (Game.citiesChecked[cities] === val) {
-                        alert('Этот город уже был назван!');
-                        return false;
+                for (var cities in citiesChecked) {
+                    lastElem = citiesChecked[citiesChecked.length-1];
+                    lastLetter = lastElem[lastElem.length-1];
+                    if (lastLetter === "ъ" ||
+                        lastLetter === "ь" ||
+                        lastLetter === "ы") {
+                        lastLetter = lastElem[lastElem.length-2];
                     }
-                    
-                    
-                }
-                
-                var lastLetter = "";
-                if (Game.AIResponse === "" && 
-                    Game.AIResponse === undefined &&
-                    Game.AIResponse === null) {
-                    Game.AIResponse.innerText = val;
-                    
-                    
-//                    for (var city in Game.citiesChecked) {
-//                        for (var l = 0; l < Game.citiesChecked[city].length; l++) {
-//                            if (l === (Game.citiesChecked[city].length - 1)) {
-//                                lastLetter = Game.citiesChecked[city][l];
-//                            }
-//                        }
-//                    }
-                    
-                    if (val[0] === lastLetter) {
-                        Game.citiesChecked.push(val);
-                        return true;
+                    for (var c in citiesChecked) {
+                        if (citiesChecked[c] === val) {
+                            alert('Этот город уже был назван!');
+                            return false;
+                            }
+                        }
+                    if (firstLetter === lastLetter) {
+                            citiesChecked.push(val);
+                            ymaps.ready(function() {
+                                ymaps.geocode(val);
+                            });
+                            Game.prototype.AIResponseField.innerText = val;
+                            Game.prototype.input.value = "";
+                            return true;
+                        } else {
+                            alert("Город должен начинаться на последнюю букву предыдущего: " + Game.prototype.AIResponseField.innerText)
+                        }
                     }
                 }
             }
-        })
+        });
     }
     
 }
