91bbd79c94c54e6c1387f3622cee218c10e55737
Alex Naidovich <alexnaidovich@users.noreply.github.com>
2018-02-18 23:16:52 +0300

    0.3.1
    Getting database from github repository

--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ Game.prototype = {
         var gameData = Game.prototype.testGameData,
             content = "",
             xhr = new XMLHttpRequest();
-        var url = "../data/cities.txt";
+        var url = "https://alexnaidovich.github.io/yandex-test-task-3/data/cities.txt";
         xhr.open("GET", url, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
